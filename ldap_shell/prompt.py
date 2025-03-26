c16c5831d64012e99d3d8adeaf1031e509b3b71e
Riocool <z-Riocool@yandex.ru>
2025-03-26 22:47:19 +0300

    chore: Remove unused helper initialization from prompt.py
    Removed the unused initialization of the Helper class in the Prompt class constructor to enhance code cleanliness and maintainability.

--- ldap_shell/prompt.py
+++ ldap_shell/prompt.py
@@ -127,7 +127,6 @@ class Prompt:
 		self.client = client
 		self.prompt = '# '
 		self.history = history
-		self.helper = Helper()
 		self.meta = self.helper.get_meta()
 		self.identchars = string.ascii_letters + string.digits + '_'
 		self.noninteractive = noninteractive
