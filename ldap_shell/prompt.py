962e7ece060c6c8ae9e812a20b1d89a0e8dad6f2
Aleksei Larionov <10401349+aspix2k@users.noreply.github.com>
2026-08-12 22:43:28 +0300

    refactor: share connection and command runner
    Move bind/TLS/LDAPS retry into session.py and module execution into
    CommandRunner. The interactive prompt uses the same runner. If a DC
    rejects plaintext LDAP because signing is required, retry over LDAPS.

--- ldap_shell/prompt.py
+++ ldap_shell/prompt.py
@@ -2,319 +2,216 @@ from prompt_toolkit.completion import Completion
 from prompt_toolkit import PromptSession
 from prompt_toolkit.auto_suggest import AutoSuggest, Suggestion
 from prompt_toolkit.shortcuts import CompleteStyle
-import string
-import logging
 from prompt_toolkit.completion import Completer
 from prompt_toolkit.key_binding import KeyBindings
+from typing import Optional
+import logging
+import shlex
+
 from ldap_shell.completers import CompleterFactory
 from ldap_shell.utils.module_loader import ModuleLoader
 from ldap_shell.utils import history
-import shlex
+from ldap_shell.runner import CommandRunner
+from ldap_shell.utils import current_sam
+
 
 class ModuleCompleter(Completer):
-	def __init__(self, modules, domain_dumper, client):
-		self.modules = modules
-		self.domain_dumper = domain_dumper
-		self.client = client
-		
-	def get_completions(self, document, complete_event):
-		text = document.text_before_cursor
-		try:
-			words = shlex.split(text)
-		except:
-			words = shlex.split(text+'"')
-		if text.endswith(' '):
-			words.append('')
-		
-		# If this is the first word - suggest modules
-		if len(words) <= 1 and not text.endswith(' '):
-			word = words[0] if words else ''
-			for module_name in self.modules:
-				if module_name.startswith(word):
-					yield Completion(
-						module_name,
-						start_position=-len(word),
-						display_meta=self.modules[module_name].__doc__
-					)
-			return
-		# Get current module and argument
-		module_name = words[0]
-		if not any(module_name.startswith(m) for m in self.modules):
-			return
-			
-		module_class = self.modules[module_name]
-		arguments = module_class.get_arguments()
-		
-		# Determine which argument needs suggestions
-		current_arg_index = max(len(words) - 2, 0)
+    def __init__(self, modules, domain_dumper, client):
+        self.modules = modules
+        self.domain_dumper = domain_dumper
+        self.client = client
 
-		if current_arg_index+1 > len(arguments):
-			return
-		
-		current_arg = arguments[current_arg_index]
-		completer = CompleterFactory.create_completer(
-			current_arg.arg_type,
-			self.client,
-			self.domain_dumper
-		)
-		if completer:
-			current_word = document.get_word_before_cursor()
-			yield from completer.get_completions(document, complete_event, current_word)
+    def get_completions(self, document, complete_event):
+        text = document.text_before_cursor
+        try:
+            words = shlex.split(text)
+        except ValueError:
+            words = shlex.split(text + '"')
+        if text.endswith(' '):
+            words.append('')
+
+        if len(words) <= 1 and not text.endswith(' '):
+            word = words[0] if words else ''
+            for module_name in self.modules:
+                if module_name.startswith(word):
+                    yield Completion(
+                        module_name,
+                        start_position=-len(word),
+                        display_meta=self.modules[module_name].__doc__
+                    )
+            return
+
+        module_name = words[0]
+        if module_name not in self.modules:
+            return
+
+        module_class = self.modules[module_name]
+        arguments = module_class.get_arguments()
+        current_arg_index = max(len(words) - 2, 0)
+        if current_arg_index + 1 > len(arguments):
+            return
+
+        current_arg = arguments[current_arg_index]
+        completer = CompleterFactory.create_completer(
+            current_arg.arg_type,
+            self.client,
+            self.domain_dumper
+        )
+        if completer:
+            current_word = document.get_word_before_cursor()
+            yield from completer.get_completions(document, complete_event, current_word)
+
 
 class ModuleSuggester(AutoSuggest):
-	"""Suggests hints from history and module arguments"""
-	
-	def __init__(self, modules, history):
-		self.modules = modules
-		self.history = history  # Should be prompt_toolkit.history.History
-		
-	def get_suggestion(self, buffer, document) -> Suggestion | None:
-		text = document.text_before_cursor
-		
-		# 1. Check history
-		history_suggestion = self._get_history_suggestion(text)
-		if history_suggestion:
-			return history_suggestion
-			
-		# 2. Suggest module arguments
-		return self._get_module_suggestion(text)
-	
-	def _get_history_suggestion(self, text: str) -> Suggestion | None:
-		"""Find last used argument for current command"""
-		if not text.strip():
-			return None
+    """Suggests hints from history and module arguments"""
 
-		# Get base command (first word)
-		base_command = text.split()[0]
-		
-		# Search history for last full command with this base name
-		last_full_command = None
-		for entry in reversed(list(self.history.get_strings())):
-			if entry.startswith(base_command + ' '):
-				last_full_command = entry
-				break
-		
-		if not last_full_command:
-			return None
-		
-		# Compare current input with history entry
-		if last_full_command.startswith(text):
-			remaining_part = last_full_command[len(text):]
-			return Suggestion(remaining_part)
-		
-		return None
-	
-	def _get_module_suggestion(self, text: str) -> Suggestion | None:
-		"""Standard module argument suggestion"""
-		words = text.split()
-		if len(words) == 0 or words[0] not in self.modules:
-			return None
-			
-		module = self.modules[words[0]]
-		args = module.get_arguments()
-		current_arg_index = len(words) - 1
-		
-		if current_arg_index >= len(args):
-			return None
-			
-		current_arg = args[current_arg_index]
-		suggestion = f"{current_arg.name} " if current_arg.required else f"[{current_arg.name}] "
-		return Suggestion(suggestion)
+    def __init__(self, modules, history):
+        self.modules = modules
+        self.history = history
+
+    def get_suggestion(self, buffer, document) -> Optional[Suggestion]:
+        text = document.text_before_cursor
+        history_suggestion = self._get_history_suggestion(text)
+        if history_suggestion:
+            return history_suggestion
+        return self._get_module_suggestion(text)
+
+    def _get_history_suggestion(self, text: str) -> Optional[Suggestion]:
+        if not text.strip():
+            return None
+        base_command = text.split()[0]
+        last_full_command = None
+        for entry in reversed(list(self.history.get_strings())):
+            if entry.startswith(base_command + ' '):
+                last_full_command = entry
+                break
+        if not last_full_command:
+            return None
+        if last_full_command.startswith(text):
+            return Suggestion(last_full_command[len(text):])
+        return None
+
+    def _get_module_suggestion(self, text: str) -> Optional[Suggestion]:
+        words = text.split()
+        if len(words) == 0 or words[0] not in self.modules:
+            return None
+        module = self.modules[words[0]]
+        args = module.get_arguments()
+        current_arg_index = len(words) - 1
+        if current_arg_index >= len(args):
+            return None
+        current_arg = args[current_arg_index]
+        suggestion = f"{current_arg.name} " if current_arg.required else f"[{current_arg.name}] "
+        return Suggestion(suggestion)
+
 
 class Prompt:
-	def __init__(self, domain_dumper, client, noninteractive=False):
-		self.domain_dumper = domain_dumper
-		self.client = client
-		self.prompt = f'{self.client.user.split("\\")[1]}# '
-		self.history = history
-		self.identchars = string.ascii_letters + string.digits + '_'
-		self.noninteractive = noninteractive
-		self.modules = ModuleLoader.load_modules()
+    def __init__(self, domain_dumper, client, noninteractive=False):
+        self.domain_dumper = domain_dumper
+        self.client = client
+        self.history = history
+        self.noninteractive = noninteractive
+        self.runner = CommandRunner(domain_dumper, client)
+        self.modules = self.runner.modules
+        self.prompt = f'{current_sam(self.client)}# '
 
-		self.completer = ModuleCompleter(self.modules, domain_dumper=self.domain_dumper, client=self.client)
-		self.suggester = ModuleSuggester(self.modules, self.history)
+        self.completer = ModuleCompleter(self.modules, domain_dumper=self.domain_dumper, client=self.client)
+        self.suggester = ModuleSuggester(self.modules, self.history)
+        self.kb = KeyBindings()
 
-		# Create key bindings
-		self.kb = KeyBindings()
-		
-		@self.kb.add('enter')
-		def _(event):
-			"""Handle Enter press"""
-			b = event.current_buffer
+        @self.kb.add('enter')
+        def _(event):
+            b = event.current_buffer
+            if b.complete_state and b.complete_state.current_completion:
+                completion = b.complete_state.current_completion
+                available_commands = ModuleLoader.list_modules()
+                if completion.text in available_commands and ' ' not in b.document.text_before_cursor:
+                    b.delete(len(b.document.text_after_cursor))
+                    b.delete_before_cursor(len(b.document.text_before_cursor))
+                    b.insert_text(completion.text + ' ')
+                else:
+                    text = b.document.text
+                    cursor_position = b.document.cursor_position
+                    text_before_cursor = text[:cursor_position]
+                    if text_before_cursor.count('"') % 2 == 1:
+                        quoted_words = shlex.split(text_before_cursor + '"')
+                    else:
+                        quoted_words = shlex.split(text_before_cursor)
+                    last_word = quoted_words[-1]
+                    if ',' in last_word and 'DC=' not in last_word:
+                        del_word = last_word.split(',')[-1]
+                    elif ' ' in last_word or '"' in last_word or 'DC=' in last_word:
+                        del_word = f'"{last_word}"'
+                    else:
+                        del_word = last_word
+                    last_separator = len(text_before_cursor) - len(del_word)
+                    if last_separator >= 0:
+                        chars_to_delete = cursor_position - last_separator
+                        if chars_to_delete > 0:
+                            b.delete_before_cursor(chars_to_delete)
+                    else:
+                        b.delete_before_cursor(len(text_before_cursor))
+                    b.insert_text(completion.text)
+                b.complete_state = None
+                return
+            event.current_buffer.validate_and_handle()
 
-			# If there is active autocompletion state
-			if b.complete_state and b.complete_state.current_completion:
-				completion = b.complete_state.current_completion
+        @self.kb.add('tab')
+        def _(event):
+            b = event.current_buffer
+            if b.complete_state:
+                b.complete_next()
+            else:
+                b.start_completion(select_first=False)
 
-				# Get list of commands
-				available_commands = ModuleLoader.list_modules()
+    def parseline(self, line):
+        return self.runner.parseline(line)
 
-				# If completion is a command
-				if completion.text in available_commands and not ' ' in b.document.text_before_cursor:
-					# Delete all text
-					b.delete(len(b.document.text_after_cursor))  # first after cursor
-					b.delete_before_cursor(len(b.document.text_before_cursor))  # then before cursor
-					# Insert command
-					b.insert_text(completion.text + ' ')
-				else:
-					# For arguments: find last space or comma before cursor
-					text = b.document.text
-					cursor_position = b.document.cursor_position
-					text_before_cursor = text[:cursor_position]
+    def is_valid_line(self, line):
+        cmd, arg, line = self.parseline(line)
+        return bool(line and cmd)
 
-					# Find position of last separator (space or comma)
-					if text_before_cursor.count('"') % 2 == 1:
-						quoted_words = shlex.split(text_before_cursor+'"')
-					else:
-						quoted_words = shlex.split(text_before_cursor)
-					
-					last_word = quoted_words[-1]
-					if ',' in last_word and not 'DC=' in last_word:
-						del_word = last_word.split(',')[-1]
-					elif ' ' in last_word or '"' in last_word:
-						del_word = f'"{last_word}"'
-					elif 'DC=' in last_word:
-						del_word = f'"{last_word}"'
-					else:
-						del_word = last_word
-					last_separator = len(text_before_cursor) - len(del_word)
+    def _parse_arg_string(self, module_name: str, arg_string: str) -> dict:
+        return self.runner.parse_args(module_name, arg_string)
 
-					if last_separator >= 0:
-						# Delete text from last separator to cursor
-						chars_to_delete = cursor_position - (last_separator)
-						if chars_to_delete > 0:
-							b.delete_before_cursor(chars_to_delete)
-					else:
-						# If no separator found, delete all text before cursor
-						b.delete_before_cursor(len(text_before_cursor))
+    def parse_module_args(self, module_name: str, arg_string: str) -> dict:
+        return self.runner.parse_args(module_name, arg_string)
 
-					b.insert_text(completion.text)
-				
-				# Clear autocompletion state
-				b.complete_state = None
-				return
+    def execute_module(self, module_name: str, args_dict: dict):
+        module = self.modules[module_name](
+            args_dict,
+            self.domain_dumper,
+            self.client,
+            logging.getLogger('ldap-shell')
+        )
+        return module()
 
-			# If no active autocompletion - execute command
-			event.current_buffer.validate_and_handle()	
+    def check_args_exist(self, module_name: str, args_dict: dict):
+        return not self.runner.missing_args(module_name, args_dict)
 
-		@self.kb.add('tab')
-		def _(event):
-			"""Handle Tab press"""
-			b = event.current_buffer
-			
-			if b.complete_state:
-				b.complete_next()
-			else:
-				b.start_completion(select_first=False)
+    def onecmd(self, line):
+        result = self.runner.execute(line, capture=False)
+        if result.prompt:
+            self.prompt = result.prompt
+        return result.prompt
 
-	def parseline(self, line):
-		line = line.strip()
-		if not line:
-			return None, None, line
-		elif line[0] == '?':
-			line = 'help ' + line[1:]
-		i, n = 0, len(line)
-		while i < n and line[i] in self.identchars: i = i+1
-		cmd, arg = line[:i], line[i:].strip()
-		return cmd, arg, line
-
-	def is_valid_line(self, line):
-		cmd, arg, line = self.parseline(line)
-		if not line:
-			return False
-		if cmd is None:
-			return False
-		self.lastcmd = line
-		if line == 'EOF' :
-			self.lastcmd = ''
-		if cmd == '':
-			return False
-		return True
-
-	def _parse_arg_string(self, module_name: str, arg_string: str) -> dict:
-		args_dict = {}
-		
-		# Use shlex for proper string parsing with quotes
-		try:
-			args = shlex.split(arg_string)
-		except ValueError as e:
-			# If there are unclosed quotes, try to process as is
-			print(f"Warning: {e}")
-			args = arg_string.strip().split()
-		
-		for i, value in enumerate(args):
-			if i >= len(self.modules[module_name].get_arguments()):
-				break
-			arg_name = self.modules[module_name].get_arguments()[i].name
-			args_dict[arg_name] = value
-		return args_dict
-
-	def parse_module_args(self, module_name: str, arg_string: str) -> dict:
-		if module_name not in self.modules:
-			raise ValueError(f"Module {module_name} not found")
-			
-		args_dict = self._parse_arg_string(module_name, arg_string)
-		return args_dict
-
-	def execute_module(self, module_name: str, args_dict: dict):
-		"""Execute module with given arguments"""
-		#try:	
-		module = self.modules[module_name](
-				args_dict,
-				self.domain_dumper,
-				self.client,
-				logging.getLogger('ldap-shell')
-			)
-		return module()
-
-	def check_args_exist(self, module_name: str, args_dict: dict):
-		module = self.modules[module_name]
-		arguments = module.get_arguments()
-		for arg in arguments:
-			if arg.name not in args_dict and arg.required:
-				return False
-		return True
-
-	def onecmd(self, line):
-		cmd, arg_string, _ = self.parseline(line)
-		if self.is_valid_line(line) is False:
-			return
-
-		if cmd in self.modules:
-			try:
-				args_dict = self.parse_module_args(cmd, arg_string)
-				if not self.check_args_exist(cmd, args_dict):
-					print(f'*** Missing required arguments for {cmd}. Use `help {cmd}` to see available arguments.')
-					return
-				return self.execute_module(cmd, args_dict)
-			except ValueError as e:
-				print(f"Error: {e}")
-				import traceback
-				print("Traceback:")
-				print(traceback.format_exc())
-		else:
-			print(f'Module {cmd} not found')
-
-	def cmdloop(self):
-		if self.noninteractive:
-			self.session = PromptSession(self.prompt, history=self.history)
-		else:
-			self.session = PromptSession(
-				self.prompt,
-				completer=self.completer,
-				complete_style=CompleteStyle.MULTI_COLUMN,
-				history=self.history,
-				auto_suggest=self.suggester, #AutoSuggestFromHistory(),
-				key_bindings=self.kb,
-				complete_while_typing=True
-			)
-		while True:
-			try:
-				line = self.session.prompt(self.prompt)
-				if line.strip() == 'exit':
-					break
-				prompt = self.onecmd(line)
-				if prompt:
-					self.prompt = prompt
-			except KeyboardInterrupt:
-				break  
+    def cmdloop(self):
+        self.session = PromptSession(
+            self.prompt,
+            completer=None if self.noninteractive else self.completer,
+            complete_style=CompleteStyle.MULTI_COLUMN,
+            history=self.history,
+            auto_suggest=None if self.noninteractive else self.suggester,
+            key_bindings=None if self.noninteractive else self.kb,
+            complete_while_typing=not self.noninteractive
+        )
+        while True:
+            try:
+                line = self.session.prompt(self.prompt)
+                if line.strip() == 'exit':
+                    break
+                prompt = self.onecmd(line)
+                if prompt:
+                    self.prompt = prompt
+            except (KeyboardInterrupt, EOFError):
+                break
---
+++ ldap_shell/runner.py
@@ -0,0 +1,186 @@
+"""Shared command runner for the interactive prompt, inline CLI and MCP."""
+from __future__ import annotations
+
+import io
+import logging
+import shlex
+import string
+import traceback
+from contextlib import redirect_stderr, redirect_stdout
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional
+
+from ldap_shell.utils.module_loader import ModuleLoader
+
+log = logging.getLogger('ldap-shell')
+
+
+@dataclass
+class CommandResult:
+    ok: bool
+    command: str
+    output: str = ''
+    error: Optional[str] = None
+    prompt: Optional[str] = None
+    data: Any = None
+
+
+class _ListHandler(logging.Handler):
+    def __init__(self):
+        super().__init__()
+        self.records: List[str] = []
+        self.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
+
+    def emit(self, record):
+        self.records.append(self.format(record))
+
+
+class CommandRunner:
+    """Parse and execute ldap_shell module commands without a TTY."""
+
+    def __init__(self, domain_dumper, client):
+        self.domain_dumper = domain_dumper
+        self.client = client
+        self.modules = ModuleLoader.load_modules()
+        self.identchars = string.ascii_letters + string.digits + '_'
+
+    def list_commands(self) -> List[Dict[str, Any]]:
+        commands = []
+        for name in sorted(self.modules):
+            module = self.modules[name]
+            commands.append({
+                'name': name,
+                'help': getattr(module, 'help_text', '') or (module.__doc__ or '').strip(),
+                'type': getattr(module, 'module_type', 'Other'),
+                'arguments': [
+                    {
+                        'name': arg.name,
+                        'required': arg.required,
+                        'description': arg.description,
+                        'type': (
+                            [item.value for item in arg.arg_type]
+                            if isinstance(arg.arg_type, list)
+                            else getattr(arg.arg_type, 'value', str(arg.arg_type))
+                        ),
+                    }
+                    for arg in module.get_arguments()
+                ],
+            })
+        return commands
+
+    def parseline(self, line: str):
+        line = (line or '').strip()
+        if not line:
+            return None, None, line
+        if line[0] == '?':
+            line = 'help ' + line[1:]
+        i, n = 0, len(line)
+        while i < n and line[i] in self.identchars:
+            i += 1
+        return line[:i], line[i:].strip(), line
+
+    def parse_args(self, module_name: str, arg_string: str) -> dict:
+        if module_name not in self.modules:
+            raise ValueError(f'Module {module_name} not found')
+        try:
+            values = shlex.split(arg_string)
+        except ValueError as exc:
+            log.warning('Argument parse warning: %s', exc)
+            values = arg_string.strip().split()
+
+        args_dict = {}
+        arguments = self.modules[module_name].get_arguments()
+        for i, value in enumerate(values):
+            if i >= len(arguments):
+                break
+            args_dict[arguments[i].name] = value
+        return args_dict
+
+    def missing_args(self, module_name: str, args_dict: dict) -> List[str]:
+        missing = []
+        for arg in self.modules[module_name].get_arguments():
+            if arg.required and arg.name not in args_dict:
+                missing.append(arg.name)
+        return missing
+
+    def execute(self, line: str, capture: bool = False) -> CommandResult:
+        cmd, arg_string, raw = self.parseline(line)
+        if not raw or not cmd:
+            return CommandResult(ok=True, command=line or '', output='')
+
+        if cmd == 'exit':
+            return CommandResult(ok=True, command=cmd, output='', data={'exit': True})
+
+        if cmd not in self.modules:
+            error = f'Module {cmd} not found'
+            if not capture:
+                print(error)
+            return CommandResult(ok=False, command=raw, error=error)
+
+        try:
+            args_dict = self.parse_args(cmd, arg_string)
+        except ValueError as exc:
+            error = str(exc)
+            if not capture:
+                print(f'Error: {error}')
+            return CommandResult(ok=False, command=raw, error=error)
+
+        missing = self.missing_args(cmd, args_dict)
+        if missing:
+            error = (
+                f'Missing required arguments for {cmd}: {", ".join(missing)}. '
+                f'Use `help {cmd}` to see available arguments.'
+            )
+            if not capture:
+                print(f'*** {error}')
+            return CommandResult(ok=False, command=raw, error=error)
+
+        stdout = io.StringIO()
+        log_handler = _ListHandler()
+        logger = logging.getLogger('ldap-shell')
+        if capture:
+            logger.addHandler(log_handler)
+
+        try:
+            module = self.modules[cmd](
+                args_dict,
+                self.domain_dumper,
+                self.client,
+                logger,
+            )
+            if capture:
+                with redirect_stdout(stdout), redirect_stderr(stdout):
+                    returned = module()
+            else:
+                returned = module()
+        except Exception as exc:
+            error = f'{exc}\n{traceback.format_exc()}'
+            if not capture:
+                print(f'Error: {exc}')
+                print(traceback.format_exc())
+            return CommandResult(ok=False, command=raw, error=str(exc), output=error)
+        finally:
+            if capture:
+                logger.removeHandler(log_handler)
+
+        if returned is False:
+            output = '\n'.join(log_handler.records + [stdout.getvalue()]).strip()
+            return CommandResult(ok=False, command=raw, output=output, error='Command failed')
+
+        prompt = returned if isinstance(returned, str) else None
+        output = '\n'.join(part for part in (*log_handler.records, stdout.getvalue()) if part).strip()
+        return CommandResult(ok=True, command=raw, output=output, prompt=prompt, data=returned)
+
+    def execute_many(self, lines: List[str], capture: bool = False) -> List[CommandResult]:
+        results = []
+        for line in lines:
+            text = line.strip()
+            if not text or text.startswith('#'):
+                continue
+            result = self.execute(text, capture=capture)
+            results.append(result)
+            if not result.ok:
+                break
+            if isinstance(result.data, dict) and result.data.get('exit'):
+                break
+        return results
